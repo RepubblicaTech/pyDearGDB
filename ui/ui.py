87c5c7e7c9dfa48df4a2095ade7d3abc210d0756
RepubblicaTech <76620155+RepubblicaTech@users.noreply.github.com>
2025-12-12 18:58:24 +0100

    (finally) a layout prototype

--- ui/ui.py
+++ ui/ui.py
@@ -13,11 +13,27 @@ class pyGDBApp:
     def create(self):
         dpg.create_viewport(title='dearGDB', width=1200, height=800)
         
-        self.codeWindow = dpg.window(label="Code View")
+        self.symWindow = dpg.window(label="Variables", width=200, height=400)
+        self.stackWindow = dpg.window(label="Stack", pos=(200, 0), width=200, height=400)
+        self.codeWindow = dpg.window(label="Code View", pos=(400, 0), width=800, height=500)
+        
+        self.cpuWindow = dpg.window(label="CPU Registers", pos=(0, 400), width=400, height=400)
+        self.memWindow = dpg.window(label="Memory", pos=(400, 500), width=800, height=300)
+        
         with self.codeWindow:
-            dpg.add_text("Source code + disassembly goes here")
+            dpg.add_text("Source code and/or disassembly")
             
-        dpg.show_font_manager()
+        with self.cpuWindow:
+            dpg.add_text("CPU registers")
+            
+        with self.stackWindow:
+            dpg.add_text("Stack view")
+            
+        with self.symWindow:
+            dpg.add_text("Variables")
+            
+        with self.memWindow:
+            dpg.add_text("Memory")
 
         dpg.setup_dearpygui()
         dpg.show_viewport()
